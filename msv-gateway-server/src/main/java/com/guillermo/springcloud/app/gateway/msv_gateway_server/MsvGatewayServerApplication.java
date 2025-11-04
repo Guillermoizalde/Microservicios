12e53baa05a9c13a759550f4c02dd491625b5ace
Guillermo Izalde <izalde.guillermo45@gmail.com>
2025-11-04 12:41:59 -0600

    Creación de API-gateway

---
+++ msv-gateway-server/src/main/java/com/guillermo/springcloud/app/gateway/msv_gateway_server/MsvGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.guillermo.springcloud.app.gateway.msv_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvGatewayServerApplication.class, args);
+	}
+
+}
---
+++ msv-gateway-server/src/test/java/com/guillermo/springcloud/app/gateway/msv_gateway_server/MsvGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillermo.springcloud.app.gateway.msv_gateway_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
